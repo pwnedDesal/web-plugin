e6f77639a0407b7fb6e86568f5379b30b584dbb7
mar adrian belen <mardrianbelen@gmail.com>
2015-07-31 17:25:39 +0800

    Create mimic.js

---
+++ mimicgit/mimic.js
@@ -0,0 +1,204 @@
+github_source_code=[
+{'source_uri':'https://gist.githubusercontent.com/deadstar1/77ab3e3b5f152fb77058/raw/91b9ef54efefc986c09cb3f02a44465a3b329942/test.html','cont_ID':'a'},
+{'source_uri':'https://gist.githubusercontent.com/deadstar1/77ab3e3b5f152fb77058/raw/91b9ef54efefc986c09cb3f02a44465a3b329942/test.html','cont_ID':'b'}
+];
+
+i=0
+while(github_source_code[i]){
+	alert(github_source_code[i].source_uri);
+	alert(github_source_code[i].cont_ID);
+	init_class(github_source_code[i].source_uri,github_source_code[i].cont_ID);
+	i=i+1;
+}	
+
+function createCORSRequest(method,url){
+		var xhr = new XMLHttpRequest();
+		//xhr for chorme/firefox/opera/safari.
+		if ("withCredentials" in xhr){
+			xhr.open(method,url,true);
+		}
+		else if (typeof XDomainRequest !="undefined"){
+			//xdomainrequest for IE.
+			xhr = new XDomainRequest();
+			xhr.open(method,url);
+
+		}
+		else {
+			//cors not supported
+			xhr= null;
+		}
+		return xhr;
+	}
+function syntax_highlighter(htmlcode){
+	//var exp=/\<\/{0,1}(\w)\>/g;
+	//    &lt;\/{0,1}(\w)&gt;
+		syntax_highlighter=[
+		{"replacement":"<span style='color:#a6e22e'>$1</span>","regex":"(&quot;.{1,10}&quot;)"},
+		{"replacement":"<span style='color:#66D9EF'>$1</span>","regex":"(include|package|this)"},
+		{"replacement":"<span style='color:#66D9EF'>$1</span>","regex":"(echo)"},
+		{"replacement":"<span style='color:#a6e22e'>$1</span>","regex":"(new)"},
+		{"replacement":"<span style='color:#F3933B'>$1</span>","regex":"(isset|long|short|if|var|for|while|do while)"},
+		{"replacement":"<span style='color:#a6e22e'>$1</span>","regex":"(&apos;.{1,10}&apos;)"},
+		{"replacement":"<span style='color:#F3933B'>$1<span style='color:#FFD700'>$2<span style='color:white'>$3</span></span></span>","regex":"(function)(.+)(\{)"},
+		{"replacement":"<span style='color:#00FFAC'>$1</span>","regex":"(&lt;|&gt;)"}
+		//{"replacement":"<span style='color:#00FFAC'>$1</span>","regex":"&lt;(.)+&gt;"}
+
+		];
+	x=0;
+	while(syntax_highlighter[x]){
+        var re= new RegExp(syntax_highlighter[x].regex,'g');
+		htmlcode=htmlcode.replace(re,syntax_highlighter[x].replacement);
+		x=x+1;
+		console.log("loop of syntax_highlighter" + x);
+      
+
+		}
+		alert("debug:" + htmlcode);
+	return htmlcode;
+
+}
+
+function mimic_git(text_response,randomstring,username,filename){
+	var regex_code_line=/((.{0,})(\s)?)/g;
+	text=text_response;
+	var tableofcontent=document.getElementById('tableofcontent-' + randomstring);
+	var codex=text.match(regex_code_line); ///////here
+	alert('fucking codex ' + codex.length);
+	alert(codex);
+	
+	var FinalHTMLcode='';
+	console.log('alert block of code is comming!');
+	for (var i = 0; i < codex.length; i++) {
+		//alert(codex[i]);
+		raw_htmlcode=codex[i].replace(/\</g,"&lt;").replace(/\>/g,"&gt;").replace(/\'/g,"&apos;").replace(/\"/g,"&quot;").replace(/\n/g,"&NewLine;").replace(/\s/g,"&nbsp;");
+		//raw_htmlcode1=syntax_highlighter(raw_htmlcode);
+		/////////////		////////////////////////////////////////////////
+			///////////////////////////////////////////////solution: HTML code the special chart(look up!)
+
+	
+
+		////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////
+
+		///////////////////////////////////////////////
+
+
+		HTMLcode="<tr><td class='line-number'>"+ i +"</td><td class='code-content'><code>"+ raw_htmlcode +"</code></td></tr>";
+		//some check here 
+		//////
+		alert("line of " + i +  " " + HTMLcode);
+		FinalHTMLcode=FinalHTMLcode + HTMLcode; // update the finalhtmlcode variable
+		console.log(FinalHTMLcode);
+	};
+		/**while(codex[i]){//check line by line.
+		//alert(codex[i]);
+		raw_htmlcode=codex[i].replace(/\</g,"&lt;").replace(/\>/g,"&gt;").replace(/\'/g,"&apos;").replace(/\"/g,"&quot;").replace(/\n/g,"&NewLine;").replace(/\s/g,"&nbsp;");
+		//raw_htmlcode1=syntax_highlighter(raw_htmlcode);
+		/////////////		////////////////////////////////////////////////
+		///////////////////////////////////////////////solution: HTML code the special chart(look up!)
+
+	
+
+		////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////
+
+		///////////////////////////////////////////////
+
+
+		HTMLcode="<tr><td class='line-number'>"+ i +"</td><td class='code-content'><code>"+ raw_htmlcode +"</code></td></tr>";
+		//some check here 
+		//////
+		alert("line of " + i +  " " + HTMLcode);
+		FinalHTMLcode=FinalHTMLcode + HTMLcode; // update the finalhtmlcode variable
+		console.log(FinalHTMLcode);
+
+
+		i=i+1;
+
+
+		}**/
+		//highligth
+
+		var FinalHTMLcode=syntax_highlighter(FinalHTMLcode);
+		tableofcontent.innerHTML=FinalHTMLcode;
+		//metadata
+		metadata=filename + " <span class='by'>by</span><a class='link' href=https://github.com/"+ username +">"+ username + "</a> &#32;&#32;|&#32;&#32;Mimic_Git";
+		var div_metadata=document.getElementById('user-info');
+		div_metadata.innerHTML=metadata;
+			
+
+}
+
+//init here
+	//get the <script> tag which contain this script by using getelement and checking the source attribute
+	//create a new div tag with id mimic 
+	//and make it as a sibling of <script> tag.
+	//use random math to generate unique ID for the table.
+	//encode some special char
+function init_class(github_uri,class_cont){
+	alert("githuburi:"+github_uri)
+	var url=github_uri;
+	regex_randomstring=/\w+\/(\w+)\/raw/;
+	randomstring=url.match(regex_randomstring)[0];
+
+	//var x = document.getElementsByTagName("script");
+	//loc=0;
+	//i=0;
+	//while(x[i]){
+	//alert(x[i].getAttribute('src'));
+	//regex=/(noob.js)$/g;
+	//if(regex.test(x[i].getAttribute('src'))){
+	//	console.log("location of script tag" + x[i] + " found at " + i);
+	//	loc=i;
+	//break;
+	//}
+	//	i=i+1;
+	//}
+
+	//console.log(x[loc].parentElement); //parent of selected <script> tag
+
+	//creating a new child
+	//newdiv=document.createElement('div');
+	//newdiv.setAttribute('id','mimic-' + randomstring);
+	//x[loc].parentElement.insertBefore(newdiv,x[loc]);
+	//########################////
+
+	//var targetobj=document.getElementById('mimic-'+ randomstring);
+	alert('class_cont:' + class_cont );
+	var targetobj=document.getElementById(class_cont);
+	//init here
+	//get the table unique table id 
+	var htmlobj="<div class='flexy'><div class='body'><span class='footcommand'><span>Toggle:</span><span class='hiddenT'><a class='link' href="+url+">&lt;/viewraw&gt;</a>|copy me</span></span><table class='tableofcontent' id='tableofcontent-"+ randomstring +"'></table></div><div class='footer'><span class='club'>&clubs;&#32;&#32; &#32; </span><span class='user-info' id='user-info'></span></div></div>";
+
+	targetobj.innerHTML=htmlobj;
+	var regex_filename=/(\/.{4,15}\.(.{4}$|.{3}$))/;
+	var regex_username=/(\/.{5,20}\/)/;
+	var filename=url.match(regex_filename)[0];
+	var username=url.match(regex_username)[0];
+	var regex_url=/^(https:\/\/gist.githubusercontent.com)\/(\w+)\/(\w+)/;
+	var final_url=url.match(regex_url)[0] + '/raw';
+	alert(final_url);
+	//inserting the code to your table.
+	
+	//CORS request
+	var xhr=createCORSRequest('GET',final_url);
+	if(!xhr){
+		alert('cors not supported');
+		}
+		//response handlers.
+		xhr.onload=function(){
+			var text = xhr.responseText;
+			//alert(text);
+			mimic=mimic_git(text,randomstring,username,filename);
+
+
+		};
+		xhr.onerror=function(){
+			alert('some error, you are on offline test  mode');
+			
+
+		}
+		xhr.send();
+
+}
+	
